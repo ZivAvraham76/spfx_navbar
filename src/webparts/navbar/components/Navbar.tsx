cde047cbd1a363afe444b51330f68f2738390c46
Ziv Avraham <ziva@checkpoint.com>
2025-04-08 16:05:42 +0300

    fix problem with onedrive

--- src/webparts/navbar/components/Navbar.tsx
+++ src/webparts/navbar/components/Navbar.tsx
@@ -10,7 +10,7 @@ import Acronyms from './Acronyms';
 const Navbar: React.FC<INavbarProps> = (props) => {
 
   return (
-    <div className="w-full max-w-[1230px] mx-auto flex items-center justify-start h-24 bg-white shadow-custom-shadow px-8 py-3 gap-[24px]">
+    <div className="widescreen:max-w-full laptop: mx-auto flex items-center justify-start h-24 bg-white shadow-custom-shadow px-8 py-3 gap-[24px]">
         <LeftNav />
         <Search />
         <Services />
